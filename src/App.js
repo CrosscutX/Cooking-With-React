3cc3bb6120d13e5c3b4e1758172ec17a303dc5e5
CrosscutX <crosscutcode@gmail.com>
2023-01-16 17:03:04 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import Counter from "./Counter";
+import CounterHooks from "./CounterHooks";
+
+export const ThemeContext = React.createContext();
 
 function App() {
+  console.log("Render App");
+  const [theme, setTheme] = useState("red");
   return (
-    <div>
-      <button className="btn">-</button>
-      <span>0</span>
-      <button>+</button>
-    </div>
+    <ThemeContext.Provider value={{ backgroundColor: theme }}>
+      Counter
+      <Counter initialCount={0} />
+      Counter Hooks
+      <CounterHooks initialCount={0} />
+      <button
+        onClick={() =>
+          setTheme((prevTheme) => {
+            return prevTheme === "red" ? "blue" : "red";
+          })
+        }
+      >
+        Toggle Theme
+      </button>
+    </ThemeContext.Provider>
   );
 }
 
